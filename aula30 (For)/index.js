d7bf32800fdf3c2ed873341c4e359431d85a3255
Pedro Figueiredo <pedrohfigs@gmail.com>
2022-12-26 18:35:20 -0300

    aula30

---
+++ aula30 (For)/index.js
@@ -0,0 +1,12 @@
+const frutas = ['Maça', 'Pera', 'Uva', 'Banana', 'Abacate', 'Abacaxi', 'Limão'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`,frutas[i]);
+}
+
+
+
+//for (let i = 0; i <= 10; i++) {
+//    const par = i % 2 === 0 ? 'par' : 'impar';
+//    console.log(i,par);
+//}
