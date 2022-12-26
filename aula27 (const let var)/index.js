02a88a92843f39691da759736f5c9b3fe2b24899
Pedro Figueiredo <pedrohfigs@gmail.com>
2022-12-26 17:44:23 -0300

    aula28

---
+++ aula27 (const let var)/index.js
@@ -0,0 +1,35 @@
+const verdadeira = true;
+
+//let nome = 'Luiz';
+//var nome2 = 'Luiz';
+
+//var nome2 = 'Otávio';
+
+
+//if (verdadeira) {
+//    let nome = 'Otávio';
+//    console.log(nome, nome2);
+//
+//    if(verdadeira){
+//        let nome = 'Outra coisa';
+//        console.log(nome,nome2);
+//    }
+//}
+
+
+//function falaOi(){
+//    if(verdadeira){
+//        let nome = 'Luiz';
+//        var sobrenome = 'Miranda';
+//    }
+//    console.log(sobrenome); contando que a variavel var esteja
+//dentro função ela pode ser acessada fora do bloco, diferentemente da let.
+//
+//}
+
+
+//falaOi();
+
+console.log(sobrenome); //nesse caso vai retornar undefined, se for let vai apresentar um erro pois a variavel foi utilizada antes de ser definida
+
+var sobrenome = 'Miranda';
---
+++ aula28 (Atribuição via desestruturação Arrays)/index.js
@@ -0,0 +1,25 @@
+//                  0       1       2
+//                0 1 2   0 1 2   0 1 2
+const numeros = [[1,2,3],[4,5,6],[7,8,9]]
+const [lista1, lista2, lista3] = numeros;
+console.log(lista1[2]);
+
+
+
+//const numeros = [1000,2000,3000,4000,5000,6000,7000,8000,9000];
+
+//const [um, , tres, ,cinco, , sete, ...resto] = numeros;
+//console.log(um, tres, cinco, sete);
+//console.log(resto);
+
+
+
+
+//let a = 'A';
+//let b = 'B';
+//let c = 'C';
+
+//const letras = [b, c, a];
+//[a, b, c] = letras;
+
+//console.log(a,b,c);
